c899a9edde2bd25bd92ca508e4e25a6ad6326f35
290_100008737535_winter <290_100008737535_winter@git.cloud.tencent.com>
2019-03-20 11:31:37 +0800

    添加文件

---
+++ NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  GJMetalNotification
+//
+//  Created by Miller on 2018/11/20.
+//  Copyright © 2018 GP. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
